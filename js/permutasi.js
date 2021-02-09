65d176b55bea36907df7e9ccffe0a15fd48f6c32
Agung prasetio <agungprasetio878@gmail.com>
2021-02-09 21:55:47 +0700

    permutasi

---
+++ js/permutasi.js
@@ -0,0 +1,67 @@
+const penyelesain = document.getElementById('penyelesain');
+const form = document.getElementById('form');
+const n = document.getElementById('n');
+const k = document.getElementById('k');
+
+
+const mencariFaktorial = num => {
+  return num === 1 ? 1 : (num * mencariFaktorial(num - 1))
+}
+
+const mencariPermutasi = (penyebut,pembilang) => {
+  return penyebut/pembilang;
+}
+
+function domPenyelesaian(n,k, penyebut, pembilang, hasil){
+  let html = `
+    <table>
+      <tr>
+        <td class="garis">${n}!</td>
+      </tr>
+      <tr>
+        <td>(${n} - ${k})!</td>
+      </tr>
+
+      <tr>
+        <td class="garis">${n}!</td>
+      </tr>
+      <tr>
+        <td>${n-k}!</td>
+      </tr>
+
+      <tr>
+        <td class="garis">${n}!</td>
+      </tr>
+      <tr>
+        <td>${n-k}!</td>
+      </tr>
+
+      <tr>
+        <td class="garis">${penyebut}</td>
+      </tr>
+      <tr>
+        <td>${pembilang}</td>
+      </tr>
+
+      <tr>
+        <td>${hasil}</td>
+      </tr>
+    </table>
+  `
+  penyelesain.innerHTML = html;
+}
+
+function eventMencariPermutasi(e){
+  e.preventDefault();
+  if (n.value.trim() === '' || k.value.trim() === '' ){
+    alert('tambahkan nilai n dan k');
+  } else {
+    const penyebut = mencariFaktorial(n.value);
+    const pembilang = mencariFaktorial(n.value - k.value);
+    const hasil = mencariPermutasi(penyebut, pembilang);
+    console.log(pembilang);
+    domPenyelesaian(n.value, k.value, penyebut, pembilang, hasil);
+  }
+}
+
+form.addEventListener('submit', eventMencariPermutasi);
